7c0ff02d859df2625d7b3515b03cf65fd724799d
Jamal Paden <j.paden12@yahoo.com>
2017-09-30 18:56:39 -0400

    Adding User class and UserType enum

---
+++ app/src/main/java/cs2340/theratpack/ratapp/UserType.java
@@ -0,0 +1,25 @@
+package cs2340.theratpack.ratapp;
+
+/**
+ * Created by jpade on 9/30/2017.
+ */
+
+public enum UserType {
+    USER("User"),
+    ADMIN("Administrator");
+
+    private String value;
+
+    private UserType(String value) {
+        this.value = value;
+    }
+
+    /**
+     * Method to return the string representation of the UserType
+     * @return the string representation of the enum
+     */
+    @Override
+    public String toString() {
+        return value;
+    }
+}
