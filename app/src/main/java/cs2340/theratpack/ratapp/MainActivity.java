fa8485b03c79eab5f234c881e41ad77481bfd46c
Jamal Paden <j.paden12@yahoo.com>
2017-10-16 16:46:38 -0400

    Starting on the MainActivity

--- app/src/main/java/cs2340/theratpack/ratapp/MainActivity.java
+++ app/src/main/java/cs2340/theratpack/ratapp/MainActivity.java
@@ -2,16 +2,39 @@ package cs2340.theratpack.ratapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseListAdapter;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Firebase stuff
+     */
+    private DatabaseReference ref;
+
+    /**
+     * UI Stuff
+     */
+    RecyclerView ratList;
+    FirebaseListAdapter adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ref = FirebaseDatabase.getInstance().getReference();
+        ratList = (RecyclerView) findViewById(R.id.ratList);
+
+        assert ratList != null;
+
 
         Button logout = (Button) findViewById(R.id.logout_button);
         logout.setOnClickListener(new View.OnClickListener() {
@@ -28,6 +57,48 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
+    }
+
+    public void setupRecyclerView(@NonNull RecyclerView view) {
+        view.setAdapter(new RecyclerViewAdapter(SimpleModel.INSTANCE.getItems()));
+    }
+
+    public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+        private final List<Rat> mValues;
+
+        public RecyclerViewAdapter(List<Rat> items) { mValues = items; }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.activity_main, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.rat
+        }
+
+
+
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public Rat mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = ()
+            }
+        }
+    }
+
+    public void readData() {
+
     }
 
 }
---
+++ app/src/main/java/cs2340/theratpack/ratapp/RatSighting.java
@@ -0,0 +1,13 @@
+package cs2340.theratpack.ratapp;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+/**
+ * Created by Jamal Paden on 10/09/2017.
+ */
+
+public class RatSighting {
+
+
+}
