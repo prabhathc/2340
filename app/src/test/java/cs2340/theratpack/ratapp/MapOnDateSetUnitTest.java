107c9a1a3b3097c4de0f874456b24edba1c668c1
Fungtu <a.fungtu@gmail.com>
2017-12-03 17:37:18 -0500

    I have added custom Icons to the navigation drawer, ty isabel

---
+++ app/src/test/java/cs2340/theratpack/ratapp/MapOnDateSetUnitTest.java
@@ -0,0 +1,16 @@
+package cs2340.theratpack.ratapp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Fungtu on 11/13/17.
+ */
+
+public class MapOnDateSetUnitTest extends Test {
+    @Test
+    public void noNullWorks() throws Excpetion  {
+        assertEquals(null,  );
+
+    }
+}
